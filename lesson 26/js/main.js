4cb94d2a7f3299339652c6596f77ec458db1c717
rudiinaa <grajqevcirudina@gmail.com>
2025-05-10 14:43:47 +0200

    lesson 26

---
+++ lesson 26/js/main.js
@@ -0,0 +1,101 @@
+console.log("hello there")
+
+var x=5;
+console.log(x);
+
+var y=100;
+console.log(x+y);
+
+var a = 20
+var b = 30
+var c = a + b
+
+console.log(c)
+
+var r=60
+var z=2
+var c=60/2
+
+console.log(c)
+
+var l=2
+var c=50
+var k=2*50
+
+console.log(k)
+
+n=3
+s=2
+rezultati= n%s
+console.log(rezultati)
+
+console.log(8%2)
+
+numri=70
+numri--;
+numri--;
+console.log(numri)
+
+numridyt=50
+numridyt++;
+numridyt++;
+numridyt++;
+console.log(numridyt)
+console.log("------------")
+var y=20;
+y-=10
+
+console.log(y)
+console.log("------------")
+var x=15
+x+=5
+
+console.log(x)
+console.log("------------")
+var k=30;
+k*=2
+
+console.log(k)
+console.log("------------")
+var l=60;
+l/=3
+
+console.log(l)
+console.log("-----pjesa e dyte-------")
+
+var x=5;
+var y=5;
+
+console.log(x==y)
+
+var e=6;
+var k=7;
+
+console.log(e==k)
+console.log("---------------")
+
+var p= 16;
+var n= 17;
+
+console.log(p!=n)
+console.log("---------------")
+
+var num1=60;
+var num2=100;
+
+console.log(num1<num2)
+console.log(num1>num2)
+
+var age=16;
+
+console.log(age>10 && age<30)
+
+var var1=6;
+var var2=9;
+
+console.log(var1==5 || var2==9)
+
+var firstName="Rudina"
+var lastName="Grajqevci"
+
+console.log(firstName+" "+lastName)
